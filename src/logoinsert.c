50a005caeb60f7b0644dc8fb24149107178d2dcc
xBiggs <42427630+xBiggs@users.noreply.github.com>
2023-08-29 20:15:08 -0400

    Update logoinsert.c
    Updated to support modern C (C99, C11, C17, C2x), added error handling to libc functions, and computed mr file size without invoking undefined behavior.

--- src/logoinsert.c
+++ src/logoinsert.c
@@ -27,52 +27,58 @@
  *
  */
 
+#if __STDC_VERSION__ < 199901L
+  #error "Minimum ISO C99 Required"
+#elif __STDC_VERSION__ == 199901L
+  #define noreturn
+#elif __STDC_VERSION__ <= 201710L
+  #include <stdnoreturn.h>
+#elif __STDC_VERSION__ > 201710L
+  #define noreturn [[noreturn]]
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-    FILE *mr, *ip;
-    int i;
-    char *data;
-    int length;
+static FILE* mr = (void*) 0;
+static FILE* ip = (void*) 0;
+static char* data = (void*) 0;
 
-    printf("logoinsert 0.1 by adk/napalm\n");
+static inline void cleanup(void) {
+  if (mr && (fclose(mr) == EOF)) fprintf(stderr, "Failed to close mr file.\n");
+  if (ip && (fclose(ip) == EOF)) fprintf(stderr, "Failed to close ip file.\n");
+  free(data);
+}
 
-    if (argc != 3) {
-        printf("%s <image.mr> <ip.bin>\n",argv[0]);
-        exit(0);
-    }
+noreturn static inline void error(const char* string) {
+  perror(string);
+  exit(EXIT_FAILURE);
+}
 
-    mr = fopen(argv[1], "rb");
-    ip = fopen(argv[2], "rb+");
+int main(int argc, char* argv[argc + 1]) {
+  if (atexit(cleanup)) error("atexit() registration failed");
+  switch (argc) {
+    case 3: {
+      puts("logoinsert 0.1 by adk/napalm");
 
-    if (!mr) {
-        perror(argv[1]);
-        exit(0);
-    }
+      if (!(mr = fopen(argv[1], "rb"))) error(argv[1]);
+      if (!(ip = fopen(argv[2], "rb+"))) error(argv[2]);
 
-    if (!ip) {
-        perror(argv[2]);
-        exit(0);
-    }
+      size_t size = 0;
+      while (fgetc(mr) != EOF) size++;
+      if (ferror(mr)) error(argv[1]);
+      else if (!feof(mr)) error(argv[1]);
 
-    fseek(ip, 0x3820, SEEK_SET);
+      if (size > 8192) puts("Warning: this image is larger than 8192 bytes and will corrupt a normal ip.bin, inserting anyway!");
 
-    fseek(mr, 0, SEEK_END);
-    length = ftell(mr);
-    fseek(mr, 0, SEEK_SET);
+      if (!fseek(ip, 0x3820, SEEK_SET)) error(argv[2]);
+      if (!fseek(mr, 0, SEEK_SET)) error(argv[1]);
 
-    if (length > 8192){
-        printf("Warning: this image is larger than 8192 bytes and will corrupt a normal ip.bin, inserting anyway!\n");
-    }
-
-    data = (char *)malloc(length);
-
-    fread(data, length, 1, mr);
-
-    fwrite(data, length, 1, ip);
-
-    fclose(ip);
-    fclose(mr);
+      if (!(data = calloc(size, sizeof(*data)))) error("calloc() failed");
+      if (fread(data, sizeof(*data), size, mr) < size) error("fread() failed");
+      if (fwrite(data, sizeof(*data), size, ip) < size) error("fwrite() failed");
+      return EXIT_SUCCESS;
+    } break;
+    default: return fprintf(stderr, "%s <image.mr> <ip.bin>\n", argv[0]), EXIT_FAILURE;
+  }
 }
